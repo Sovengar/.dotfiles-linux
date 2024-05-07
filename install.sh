be7ac6b44d5b41ed0c1a8e8c3d287e7c398ca98b
Sovengar <bubles_2009@hotmail.com>
2024-05-07 22:47:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ chmod a+x ~/.local/bin/yadm
 
 # Extract dotfiles from the repo to their respective locations on the system
 # Executes bottstrap file which is the file that contains the commands for bootstrapping
-~/.local/bin/yadm clone --bootstrap -f https://github.com/marcogreiveldinger/.dotfiles.git
+~/.local/bin/yadm clone --bootstrap -f https://github.com/sovengar/.dotfiles-linux.git
 
 # Clean up by removing yadm
 rm -rf ~/.local/bin/yadm
